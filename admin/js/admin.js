ea25dd247711d98c25e0946701eefa2ebccbb19f
Rodney Lacambra <29935047+rodneylacambra@users.noreply.github.com>
2026-05-16 12:36:10 +0800

    Add files via upload

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,1 +1,29 @@
+/**
+ * Holy Rosary - Admin JavaScript
+ *
+ * @package HolyRosary
+ */
 
+/* global holyRosaryAdmin, jQuery */
+( function ( $ ) {
+	'use strict';
+
+	$( document ).ready( function () {
+
+		// Confirm before moderating intentions.
+		$( document ).on( 'submit', '.holy-rosary-admin form', function ( e ) {
+			const action = $( this ).find( '[name="intention_action"]' ).val();
+			if ( ! action ) return;
+
+			const msg = action === 'approved'
+				? holyRosaryAdmin.i18n.confirmApprove
+				: holyRosaryAdmin.i18n.confirmReject;
+
+			if ( ! window.confirm( msg ) ) {
+				e.preventDefault();
+			}
+		} );
+
+	} );
+
+} )( jQuery );
